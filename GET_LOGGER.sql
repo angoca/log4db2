52c82393a23dfcae4da4d61fb0474d1b78f0ec78
Andres Gomez Casanova <angoca@yahoo.com>
2012-09-19 16:02:20 -0500

    If check fixed

--- GET_LOGGER.sql
+++ GET_LOGGER.sql
@@ -46,7 +46,7 @@ ALTER MODULE LOGGER ADD
   WHERE C.LOGGER_ID = 0;
   -- TODO To check the value defaultRootLevel before assign Warn as default.
   -- If the root logger is not defined, then set the default level: WARN-3.
-  IF (LEVEL_ID IS NULL) THEN
+  IF (PARENT_LEVEL IS NULL) THEN
    SET PARENT_LEVEL = 2;
   END IF;
   
