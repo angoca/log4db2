6c190c2ddfc2ea5560ed6a7cc70b79876f818e1a
Andres Gomez Casanova <angoca@yahoo.com>
2012-10-09 10:51:48 -0500

    Cache controlled with param

--- Tools.sql
+++ Tools.sql
@@ -1,6 +1,10 @@
 --#SET TERMINATOR @
 SET CURRENT SCHEMA LOGGER_1A @
 
+-- Constanct internalCache
+ALTER MODULE LOGGER ADD
+  VARIABLE INTERNAL_CACHE ANCHOR LOGDATA.CONFIGURATION.KEY CONSTANT 'internalCache' @
+
 -- Constant logInternals
 ALTER MODULE LOGGER ADD
   VARIABLE LOG_INTERNALS ANCHOR LOGDATA.CONFIGURATION.KEY CONSTANT 'logInternals' @
@@ -13,6 +17,17 @@ ALTER MODULE LOGGER ADD
 ALTER MODULE LOGGER ADD
   VARIABLE VAL_TRUE ANCHOR LOGDATA.CONFIGURATION.VALUE CONSTANT 'true' @
 
+/**
+ * Variable to indicate the use of internal cache.
+ */
+ALTER MODULE LOGGER ADD
+  VARIABLE CACHE BOOLEAN DEFAULT TRUE @
+
+/**
+ * Variable that indicates if the configuration cache has been loaded.
+ */
+ALTER MODULE LOGGER ADD
+  VARIABLE LOADED BOOLEAN DEFAULT FALSE @
 /**
  * Variable for the last time the configuration was loaded.
  */
@@ -66,71 +81,99 @@ ALTER MODULE LOGGER ADD
   SECURED
  F_GET_VAL: BEGIN
   DECLARE RET ANCHOR LOGDATA.CONFIGURATION.VALUE;
-  DECLARE SECS SMALLINT;
+  DECLARE SECS SMALLINT DEFAULT 60;
   DECLARE EPOCH TIMESTAMP;
-  
-  -- Sets the quantity of seconds before refresh. 60 seconds by default.
-  REFRESH: BEGIN
-   -- Handle for the second time the function is called and the param has not
-   -- been defined.
-   DECLARE CONTINUE HANDLER FOR SQLSTATE '2202E'
+
+  -- Checks if internal cache should be used.
+  IF (CACHE = TRUE) THEN
+
+   -- Sets the quantity of seconds before refresh. 60 seconds by default.
+   REFRESH: BEGIN
+    -- Handle for the second time the function is called and the param has not
+    -- been defined.
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '2202E'
+      SET SECS = 60;
+    SET SECS = INT(CONFIGURATION[REFRESH_CONS]);
+    IF (SECS IS NULL) THEN
      SET SECS = 60;
-   SET SECS = INT(CONFIGURATION[REFRESH_CONS]);
-   IF (SECS IS NULL) THEN
-    SET SECS = 60;
-   END IF;
-  END REFRESH;
-  
-  -- Sets a reference date 1970-01-01.
-  SET EPOCH = DATE(719163);
-  IF (COALESCE(LAST_REFRESH, EPOCH) + SECS SECONDS > CURRENT TIMESTAMP) THEN
-   BEGIN
-    DECLARE VALUE ANCHOR LOGDATA.CONFIGURATION.VALUE;
-    DECLARE KEY ANCHOR LOGDATA.CONFIGURATION.KEY;
-    DECLARE AT_END BOOLEAN; -- End of the cursor.
-    DECLARE SIZE SMALLINT;
-    DECLARE POS SMALLINT;
-    DECLARE CONF CURSOR FOR
-      SELECT KEY, VALUE
-      FROM LOGDATA.CONFIGURATION;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND
-      SET AT_END = TRUE;
-
-    -- Clears the current configuration.
-    SET SIZE = CARDINALITY(CONFIGURATION_KEYS);
-    IF (SIZE >= 1) THEN
-     SET POS = 1;
-     WHILE (POS < SIZE) DO
-      SET KEY = CONFIGURATION_KEYS[POS];
-      SET CONFIGURATION[KEY] = NULL;
-      SET CONFIGURATION_KEYS[POS] = NULL;
-      SET POS = POS + 1;
-     END WHILE;
     END IF;
-    -- Reads current configuration.
-    SET POS = 1;
-    SET AT_END = FALSE;
-    OPEN CONF;
-    FETCH CONF INTO KEY, VALUE;
-    WHILE (AT_END = FALSE) DO
-     SET CONFIGURATION_KEYS[POS] = KEY;
-     SET CONFIGURATION[KEY] = VALUE;
-     SET POS = POS + 1;
-     FETCH CONF INTO KEY, VALUE;
-    END WHILE;
+   END REFRESH;
 
-    -- Sets the last configuration read as now.
-    SET LAST_REFRESH = CURRENT TIMESTAMP;
+   -- Sets a reference date 1970-01-01.
+   SET EPOCH = DATE(719163);
+   IF (COALESCE(LAST_REFRESH, EPOCH) + SECS SECONDS < CURRENT TIMESTAMP OR LOADED = FALSE) THEN
+    BEGIN
+     DECLARE VALUE ANCHOR LOGDATA.CONFIGURATION.VALUE;
+     DECLARE KEY ANCHOR LOGDATA.CONFIGURATION.KEY;
+     DECLARE AT_END BOOLEAN; -- End of the cursor.
+     DECLARE SIZE SMALLINT;
+     DECLARE POS SMALLINT;
+     DECLARE CONF CURSOR FOR
+       SELECT KEY, VALUE
+       FROM LOGDATA.CONFIGURATION;
+     DECLARE CONTINUE HANDLER FOR NOT FOUND
+       SET AT_END = TRUE;
+
+     -- Clears the current configuration.
+     SET SIZE = CARDINALITY(CONFIGURATION_KEYS);
+     IF (SIZE >= 1) THEN
+      SET POS = 1;
+      WHILE (POS < SIZE) DO
+       SET KEY = CONFIGURATION_KEYS[POS];
+       SET CONFIGURATION[KEY] = NULL;
+       SET CONFIGURATION_KEYS[POS] = NULL;
+       SET POS = POS + 1;
+      END WHILE;
+     END IF;
+     -- Reads current configuration.
+     SET POS = 1;
+     SET AT_END = FALSE;
+     OPEN CONF;
+     FETCH CONF INTO KEY, VALUE;
+     WHILE (AT_END = FALSE) DO
+      SET CONFIGURATION_KEYS[POS] = KEY;
+      SET CONFIGURATION[KEY] = VALUE;
+      SET POS = POS + 1;
+      FETCH CONF INTO KEY, VALUE;
+     END WHILE;
+     -- Sets the last configuration read as now.
+     SET LAST_REFRESH = CURRENT TIMESTAMP;
+    END;
+    -- Checks the variable for internal cache
+    BEGIN
+     -- NULL if the key is not in the configuration.
+     DECLARE EXIT HANDLER FOR SQLSTATE '2202E'
+       SET CACHE = TRUE;
+
+     SET CACHE = CASE CONFIGURATION[INTERNAL_CACHE] 
+       WHEN 'true' THEN TRUE 
+       ELSE FALSE
+       END;
+    END;
+    SET LOADED = TRUE;
+   END IF;
+
+   BEGIN
+    -- NULL if the key is not in the configuration.
+    DECLARE EXIT HANDLER FOR SQLSTATE '2202E'
+      SET RET = NULL;
+
+    SET RET = CONFIGURATION[GIVEN_KEY];
    END;
+  ELSE -- Does not use the internal cache but a query.
+   -- Checks the cache status.
+   SELECT C.VALUE INTO RET
+     FROM LOGDATA.CONFIGURATION C
+     WHERE C.KEY = INTERNAL_CACHE;
+   SET CACHE = CASE RET
+     WHEN 'true' THEN TRUE
+     ELSE FALSE
+     END;
+   SELECT C.VALUE INTO RET
+     FROM LOGDATA.CONFIGURATION C
+     WHERE C.KEY = GIVEN_KEY;
   END IF;
-  BEGIN
-   -- NULL if the key is not in the configuration.
-   DECLARE EXIT HANDLER FOR SQLSTATE '2202E'
-     SET RET = NULL;
-  
-   SET RET = CONFIGURATION[GIVEN_KEY];
-  END;
-  
+
   RETURN RET;
  END F_GET_VAL @
 
@@ -140,6 +183,9 @@ ALTER MODULE LOGGER ADD
  * configuration was loaded, and when it will be reloaded (with its frequency).
  * In the other result set, it shows the key-values from the configuration, if
  * this was already loaded, otherwise a descriptive message appears.
+ * This procedure shows the configuration, but it does not reload it, for this
+ * reason, the next refresh time could be in the past, because the get_value
+ * is the only one that refreshes the configuration.
  */
 ALTER MODULE LOGGER ADD
   PROCEDURE SHOW_CONF ()
@@ -154,7 +200,7 @@ ALTER MODULE LOGGER ADD
   -- Creates the cursor for the configuration refreshness.
   BEGIN
    DECLARE SECS SMALLINT;
-   DECLARE STMT VARCHAR(256);
+   DECLARE STMT VARCHAR(512);
    DECLARE REF CURSOR
      WITH RETURN TO CALLER
      FOR RS;
@@ -168,12 +214,15 @@ ALTER MODULE LOGGER ADD
     END IF;
    END;
    -- Creates an prepares a dynamic query.
-   SET STMT = 'SELECT LAST_REFRESH AS LAST_REFRESH, '
+   SET STMT = 'SELECT CASE CACHE WHEN TRUE '
+     || 'THEN ''true'' ELSE ''false'' END AS INTERNAL_CACHE_ACTIVATED, '
+     || 'LAST_REFRESH AS LAST_REFRESH, '
      || SECS || ' AS FREQUENCY, '
      || 'CASE ' || SECS || ' '
      || 'WHEN -1 THEN ''Not defined'' '
      || 'ELSE CHAR(LAST_REFRESH + ' || SECS || ' SECONDS) '
-     || 'END AS NEXT_REFRESH '
+     || 'END AS NEXT_REFRESH, '
+     || '''' || CURRENT TIMESTAMP || ''' AS CURRENT_TIME '
      || 'FROM SYSIBM.SYSDUMMY1';
    PREPARE RS FROM STMT;
    OPEN REF;
