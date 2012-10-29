bd4c930013c7dba32a6ffd0614eb6e4f41e77a21
Andres Gomez Casanova <angoca@yahoo.com>
2012-10-29 10:27:29 -0500

    Recreate in Windows script

---
+++ recreate.bat
@@ -0,0 +1,11 @@
+db2 -tf CleanTriggers.sql
+db2 -tf CleanObjects.sql
+db2 -tf CleanTables.sql
+db2 -tf Tables.sql
+db2 -tf Objects.sql
+db2 -td@ -f Tools.sql
+db2 -td@ -f AdminHeader.sql
+db2 -td@ -f AdminBody.sql
+db2 -td@ -f LOG.sql
+db2 -td@ -f GET_LOGGER.sql
+db2 -td@ -f Trigger.sql
