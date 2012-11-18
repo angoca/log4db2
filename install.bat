d8187f2d0a677cd440a02a4d9b2087ad04fd4441
Andres Gomez Casanova <angoca at yahoo or gmail>
2012-11-18 19:47:52 +0100

    Installer for multiple versions

--- install.bat
+++ install.bat
@@ -1,3 +1,12 @@
+
+:: Checks in which DB2 version the utility will be installed.
+:: DB2 v10.1 is the default version.
+if "%1" EQU "" goto v10.1
+if "%1" EQU "-v10.1" goto v10.1
+if "%1" EQU "-v9.7" goto v9.7
+
+:: DB2 v10.1.
+:v10.1
 db2 -tf Tables.sql
 db2 -tf Objects.sql
 db2 -td@ -f Tools.sql
@@ -6,3 +15,18 @@ db2 -td@ -f AdminBody.sql
 db2 -td@ -f LOG.sql
 db2 -td@ -f GET_LOGGER.sql
 db2 -td@ -f Trigger.sql
+goto exit
+
+:: DB2 v9.7
+:v9.7
+db2 -tf Tables.sql
+db2 -tf Objects.sql
+db2 -td@ -f Tools.sql
+db2 -td@ -f AdminHeader.sql
+db2 -td@ -f AdminBody.sql
+db2 -td@ -f LOG.sql
+db2 -td@ -f GET_LOGGER_v9_7.sql
+db2 -td@ -f Trigger.sql
+goto exit
+
+:exit
