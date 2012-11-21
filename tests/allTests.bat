a3f5093be0bc9990b0c4ea79fb0db92e7bd3396a
Andres Gomez Casanova <angoca at yahoo or gmail>
2012-11-21 23:21:52 +0100

    Insert unnecessary

---
+++ tests/allTests.bat
@@ -0,0 +1,19 @@
+call test.bat TestsAppenders.sql
+pause
+call test.bat TestsCascadeCallLimit.sql
+pause
+call test.bat TestsConfAppenders.sql
+pause
+call test.bat TestsConfiguration.sql
+pause
+call test.bat TestsConfLoggers.sql
+pause
+call test.bat TestsConfLoggersDelete.sql
+pause
+call test.bat TestsConfLoggersEffective.sql
+pause
+call test.bat TestsFunctionGetDefinedParentLogger.sql
+pause
+call test.bat TestsGetLogger.sql
+pause
+call test.bat TestsLevels.sql
