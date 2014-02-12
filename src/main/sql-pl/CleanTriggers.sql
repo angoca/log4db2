336d0fbce93853064acb75ab5bb1b46ca7f9528d
Andres Gomez Casanova <angoca@yahoo.com>
2014-02-12 11:27:43 +0100

    Update CleanTriggers.sql

--- src/main/sql-pl/CleanTriggers.sql
+++ src/main/sql-pl/CleanTriggers.sql
@@ -28,12 +28,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 SET CURRENT SCHEMA LOGGER_1A;
 
 /**
-* Drops the trigger in the opposite order as they were created.
-*
-* Version: 2014-02-14 1-Alpha
-* Author: Andres Gomez Casanova (AngocA)
-* Made in COLOMBIA.
-*/
+ * Drops the trigger in the opposite order as they were created.
+ *
+ * Version: 2014-02-14 1-Alpha
+ * Author: Andres Gomez Casanova (AngocA)
+ * Made in COLOMBIA.
+ */
 
 DROP TRIGGER T1_LOGS_UNIQUE_DATE;
 
