2469193613697afe197d31910daf6cfaecc0b774
Andres Gomez Casanova <angoca@yahoo.com>
2014-04-21 13:58:04 +0200

    Format

--- src/main/sql-pl/Trigger.sql
+++ src/main/sql-pl/Trigger.sql
@@ -28,8 +28,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 SET CURRENT SCHEMA LOGGER_1B @
 
 /**
- * Defines the triggers for the different tables. The creation order is important
- * to validate something before other thing.
+ * Defines the triggers for the different tables. The creation order is
+ * important to validate something before other thing.
  *
  * Version: 2014-02-14 1-Beta
  * Author: Andres Gomez Casanova (AngocA)
