f106cd6e0c68e8bc5c2f80eb88c6c0ddc5cea2f5
Andres Gomez Casanova <angoca@yahoo.com>
2014-08-09 16:38:39 +0200

    blank like; verbose

--- src/main/scripts/install.ps1
+++ src/main/scripts/install.ps1
@@ -22,6 +22,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+
 # Installs all scripts of the utility.
 #
 # Version: 2014-02-14 1-RC
