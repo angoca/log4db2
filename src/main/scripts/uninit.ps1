0a57897562854c73278ab5e15cc9679ca1a48d46
Andres Gomez Casanova <angoca@yahoo.com>
2014-08-29 15:46:29 +0200

    New uninit

---
+++ src/main/scripts/uninit.ps1
@@ -0,0 +1,36 @@
+# Copyright (c) 2012 - 2014, Andres Gomez Casanova (AngocA)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Cleans the environment variables.
+#
+# Version: 2014-08-29 1
+# Author: Andres Gomez Casanova (AngocA)
+# Made in COLOMBIA.
+
+# When testing, the variables are preserved.
+if ( ! ( Test-Path Variable:\LOG4DB2_SRC_TEST_CODE_PATH ) ) {
+ Remove-Variable LOG4DB2_SRC_MAIN_CODE_PATH -Scope Global
+ Remove-Variable LOG4DB2_SRC_MAIN_SCRIPT_PATH -Scope Global
+}
+
