96beb7ee97b0a1fc6970a4da11c99b26427f6f37
Andres Gomez Casanova <angoca@yahoo.com>
2022-06-09 19:47:13 -0500

    License banner

--- src/test/travis/update.sh
+++ src/test/travis/update.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
+# Copyright (c) 2014 - 2022, Andres Gomez Casanova (AngocA)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Install and/or execute a suite of tests. It receives these arguments:
+# schemaName: The name of the file in the local directory and the schema to
+# install or execute.
+# i: To install the tests suite and show logs.
+# x: To execute the tests suite.
+# If only the schema is given, it will install the tests suite.
+#
+# Version: 2016-02-01 v1
+# Author: Andres Gomez Casanova (AngocA)
+# Made in COLOMBIA.
+
 apt-get -y install libaio1 ksh libstdc++6-4.4-pic libstdc++6-4.4-dev libstdc++5 rpm libpam0g:i386 numactl
 
